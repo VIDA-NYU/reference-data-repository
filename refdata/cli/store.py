334a5a1051ca18ab8691a5ab0b86a8f57c9723e5
Heiko Mueller <heiko.muller@gmail.com>
2021-01-26 10:48:27 -0500

    Add remove dataset from local store to CLI

--- refdata/cli/store.py
+++ refdata/cli/store.py
@@ -48,6 +48,24 @@ def list_datasets(basedir, db, index):
     util.print_datasets(store.list())
 
 
+@cli_store.command(name='remove')
+@click.option('-b', '--basedir', required=False, help='Local store directory')
+@click.option('-d', '--db', required=False, help='Database connect URL')
+@click.option('-i', '--index', required=False, help='Repository index file')
+@click.option('-f', '--force', is_flag=True, default=False, help='Remove without confirmation')
+@click.argument('key')
+def remove_dataset(basedir, db, index, force, key):
+    """Remove dataset from local store."""
+    # Confirm that the user wants to remove the dataset from the local store.
+    if not force:  # pragma: no cover
+        msg = "Do you really want to remove dataset '{}'".format(key)
+        click.confirm(msg, default=True, abort=True)
+    # Read the index of given.
+    doc = util.read_index(index) if index is not None else None
+    store = LocalStore(basedir=basedir, repo=RepositoryManager(doc=doc), connect_url=db)
+    store.remove(key)
+
+
 @cli_store.command(name='show')
 @click.option('-b', '--basedir', required=False, help='Local store directory')
 @click.option('-d', '--db', required=False, help='Database connect URL')
